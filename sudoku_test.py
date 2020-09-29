0baddfc8c678789b985d2fef4527221936ca98f3
jacquelineb06 <70238481+jacquelineb06@users.noreply.github.com>
2020-09-28 18:04:59 -0700

    Add files via upload

---
+++ sudoku_test.py
@@ -0,0 +1,14 @@
+from sudoku import *
+asudoku=sudoku()
+asudoku.generate()
+asudoku.display()
+asudoku=sudoku()
+asudoku.makepuzzle(40)
+asudoku.display()
+asudoku.solved()
+asudoku.solve()
+asudoku.display() 
+
+
+
+
